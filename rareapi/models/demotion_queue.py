0f18bb96ec156c73a07bd84adfad77ebbdd6b71a
Stephen Gambill <stephensgambill@gmail.com>
2023-05-18 12:02:57 -0500

    added fixtures

--- rareapi/models/demotion_queue.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class DemotionQueue(models.Model):
-
-    action = models.CharField(max_length=500)
-    admin = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name = "admin_user")
-    approver_one = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name = "approver_user")
-
