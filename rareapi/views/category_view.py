683fe49ca7d9f9873801b8aa9d980f6759aa958c
Julia Degnan <juliagdegnan@gmail.com>
2023-05-18 15:26:14 -0500

    added category views + serializer

---
+++ rareapi/views/category_view.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Category
+
+class CategoryView(ViewSet):
+    """This will serve as the view for Categories and handle ALL methods"""
+
+    def list(self, request):
+        """Handles GET requests for all categories"""
+
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+class CategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for categories"""
+
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
