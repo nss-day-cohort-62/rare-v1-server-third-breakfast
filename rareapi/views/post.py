94a28b58c041164f97f34a5b736fc8ef9a733f8b
Adam Olson <adajolson@yahoo.com>
2023-05-19 10:08:07 -0600

    PostView added

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from rareapi.models import Post
+from rareapi.models import Post, RareUser
 
 class PostView(ViewSet):
     """Rare Post view"""
@@ -14,6 +14,7 @@ class PostView(ViewSet):
         """
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
+
         return Response(serializer.data)
 
     def retrieve(self, request, pk):
@@ -26,10 +27,16 @@ class PostView(ViewSet):
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
+class PostRareUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RareUser
+        fields =('id', 'full_name')
+
 class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
+    user = PostRareUserSerializer(many=False)
     class Meta:
         model = Post
         fields = ('id', 'user', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved', 'post_tags', "post_reaction")
-        depth = 2
+
